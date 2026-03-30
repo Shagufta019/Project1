32f125d0afb811ce63ee25696f9b9fd7c7a63c10
Shagufta <shagufta2219@gmail.com>
2026-03-30 16:36:30 +0530

    Update game logic

--- main.py
+++ main.py
@@ -5,16 +5,25 @@
 '''
 import random
 
-computer = random.choice([-1, 0, 1])
-youstr = input("Enter your choice (s,w,g): ")
+# Initialize scores
+user_score = 0
+comp_score = 0
+
 youDict = {"s":1, "w":-1, "g":0}
 reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
 
-if youstr not in youDict:
-    print("Invalid input! Please choose s, w, or g.")
-else:
-    you = youDict[youstr]
+print("--- Welcome to Snake, Water, Gun: 5 Round Match! ---")
 
+for i in range(1, 6):
+    print(f"\nRound {i}/5:")
+    computer = random.choice([-1, 0, 1])
+    youstr = input("Enter your choice (s,w,g): ")
+
+    if youstr not in youDict:
+        print("Invalid input! Please choose s, w, or g.")
+        continue
+
+    you = youDict[youstr]
     print("You chose:", reverseDict[you])
     print("Computer chose:", reverseDict[computer])
 
@@ -22,20 +31,21 @@ else:
        print("It's a draw")
 
     else:
-        if(computer == -1 and you == 1):
-            print("You win!")
-        elif(computer == 1 and you == -1):
-            print("You lose!")
-
-        elif(computer == 0 and you == 1):
-            print("You lose!")
-        elif(computer == 1 and you == 0):
-            print("You win!")
-
-        elif(computer == -1 and you == 0):
-            print("You lose!")
-        elif(computer == 0 and you == -1):
-            print("You win!")
- 
+        if(computer == -1 and you == 1) or (computer == 1 and you == 0) or (computer == 0 and you == -1):
+            print("You win this round!")
+            user_score += 1
         else:
-            print("Something went wrong, try again!")
+            print("You lose this round!")
+            comp_score += 1
+       
+# Final Result
+print("\n" + "-"*40)
+print(f"FINAL SCORE - You: {user_score} | Computer: {comp_score}")
+
+if user_score > comp_score:
+    print("CONGRATULATIONS! You won the match!")
+elif user_score < comp_score:
+    print("GAME OVER! The computer won the match.")
+else:
+    print("It's a tie match!")
+print("-"*40)
